aa08258c3d5d485737918e1dcb5e47875f8bb67c
Zelle <unconfigured@null.spigotmc.org>
2021-09-18 11:10:56 +0100

    Removed commands off header as not implimented

--- promotional/Header/Navigation.tsx
+++ promotional/Header/Navigation.tsx
@@ -1,11 +1,3 @@
-const Navigation = [
-  { name: "Home", href: "/home", includes: "home", current: false },
-  {
-    name: "Commands",
-    href: "/commands/moderation",
-    includes: "commands",
-    current: false,
-  },
-];
+const Navigation = [{ name: "Home", href: "/home", includes: "home" }];
 
 export default Navigation;
