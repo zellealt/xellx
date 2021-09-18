b63f0a1e8ec182a8184502341a5014d26fe292dc
Zelle <zeloeee@gmail.com>
2021-09-18 11:31:00 +0100

    Change Name A1.svg to Dashboard.svg

--- promotional/Layout/Hero/Right.tsx
+++ promotional/Layout/Hero/Right.tsx
@@ -2,7 +2,7 @@ const Right = () => {
   return (
     <div className="">
       <img
-        src="/A1.svg"
+        src="/Dashboard.svg"
         className="mx-10 mt-10 lg:mt-0 lg:mx-0 lg:absolute lg:-right-30 center-img pt-36 float -z-1"
       />
     </div>
