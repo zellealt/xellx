857d2024329cc81e971122dbc6451077ff3b5e67
Zelle <zeloeee@gmail.com>
2021-09-18 11:43:31 +0100

    Fix Light Theme and Dark App Overlays

--- pages/login.tsx
+++ pages/login.tsx
@@ -25,7 +25,7 @@ export default function Login() {
         <>
           <Title title="Logging in" />
           <div className="dark:bg-gray-900 bg-white opacity-75 dark:opacity-75 z-50 fixed w-full h-full top-0 left-0 flex justify-center place-items-center">
-            <div className="bg-gray-800 flex justify-center place-items-center py-5 px-10 rounded-2xl shadow-2xl">
+            <div className="dark:bg-gray-800 bg-white flex justify-center place-items-center py-5 px-10 rounded-2xl shadow-2xl">
               <Spinner />
               <h1 className="mt-2 ml-10 font-semibold">Signing you in</h1>
             </div>
