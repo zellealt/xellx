0ba88c5246ef9f170eb824c80b9b5fa4c7b66495
Zelle <unconfigured@null.spigotmc.org>
2021-09-18 11:23:40 +0100

    Finish Commands

--- promotional/Header/Navigation.tsx
+++ promotional/Header/Navigation.tsx
@@ -1,3 +1,10 @@
-const Navigation = [{ name: "Home", href: "/home", includes: "home" }];
+const Navigation = [
+  { name: "Home", href: "/home", includes: "home" },
+  {
+    name: "Commands",
+    href: "/commands/moderation",
+    includes: "commands",
+  },
+];
 
 export default Navigation;
